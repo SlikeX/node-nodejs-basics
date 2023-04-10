4bc617126509e8c5becd4f01523b3faa035d89f4
Vyacheslav_Maksyutov <vyacheslav_maksyutov@epam.com>
2023-04-10 13:54:09 +0500

    feat: add create.js solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { URL } from 'node:url';
+import {writeFile} from 'fs/promises'
+
+const fileName = 'fresh.txt';
+const str = 'I am fresh and young';
+const errMsg = 'FS operation failed';
+
 const create = async () => {
-    // Write your code here 
+    const url = getPath();
+    try {
+        await writeFile(url, str, {flag: 'wx'})
+    }catch (err){
+        console.log(err);
+        throw Error (errMsg)
+    }
 };
+function getPath() {
+    return new URL( `files/${fileName}`,import.meta.url)
+}
 
 await create();

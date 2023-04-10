da35791cc3980d1de7360d62014a8e3df7ed06e5
Vyacheslav_Maksyutov <vyacheslav_maksyutov@epam.com>
2023-04-10 14:17:12 +0500

    feat: add solution for copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { stat } from 'fs';
+import {cp} from 'fs/promises';
+
+const fileName = 'files_copy';
+const errMsg = 'FS operation failed';
+const path = 'files'
+
 const copy = async () => {
-    // Write your code here 
+    const src = getFullPath(path);
+    const copy = getFullPath(fileName);
+    try {
+        stat(src, () => {
+            console.log('Copied');
+        })
+        await cp(src, copy, {errorOnExist:true, recursive:true, force:false})
+    }catch (err){
+        console.log(err);
+        throw Error (errMsg)
+    }
 };
+function getFullPath(path) {
+    return new URL( path,import.meta.url)
+}
 
 await copy();

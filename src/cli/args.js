7f7b1e687ec86338053182e7890b0ad35db4f543
Vyacheslav_Maksyutov <vyacheslav_maksyutov@epam.com>
2023-04-10 15:36:02 +0500

    feat: add solution for ergs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here
+    const args = process.argv;
+    const newArr = args.reduce((acc, elem, index, arr)=>{
+        if (elem.startsWith('--')){
+            acc.push(`${elem.slice(2)} is ${arr[index+1]}`)
+        }
+        return acc
+    },[])
+    console.log(newArr.join(', '));
 };
 
 parseArgs();

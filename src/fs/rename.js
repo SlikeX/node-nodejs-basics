847bdee6e22c34a5efb00a969bb47b3e4bf30b9a
Vyacheslav_Maksyutov <vyacheslav_maksyutov@epam.com>
2023-04-10 15:07:29 +0500

    feat: add solution for rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import { rename as fileRename } from 'node:fs/promises';
+import { URL } from 'node:url';
+
+const errMsg = 'FS operation failed';
+const path = 'files';
+const fileName = 'wrongFilename.txt';
+const newFilename = 'properFilename.md';
+
 const rename = async () => {
-    // Write your code here 
+    const src = getFullPath(`${path}/${fileName}`);
+    const res = getFullPath(`${path}/${newFilename}`);
+    try {
+        await fileRename(src, res)
+    }catch (err){
+        throw new Error (errMsg)
+    }
+};
+function getFullPath(path) {
+    return new URL( path,import.meta.url)
 };
 
 await rename();
